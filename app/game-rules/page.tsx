1e05d0f7210130c24621906153e0d5eadbe9bfb5
bongmiin <ehvkan1@gmail.com>
2025-07-06 00:11:11 +0900

    페이지 하다 맘

---
+++ app/game-rules/page.tsx
@@ -0,0 +1,11 @@
+// app/game-rules/page.tsx
+import React from 'react';
+
+export default function GameRules() {
+  return (
+    <main>
+      <h1>게임 규칙</h1>
+      <p>여기에 게임 규칙을 작성합니다.</p>
+    </main>
+  );
+}
